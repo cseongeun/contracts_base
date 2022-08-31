7298aa434116d7c3c7cd2ae02dfa28e66a8c1e77
eun <eun@hexlant.com>
2022-08-31 12:18:17 +0900

    .

--- contracts/klaytn/kip7/features/KIP7Capped.sol
+++ contracts/klaytn/kip7/features/KIP7Capped.sol
@@ -1,4 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { KIP7Capped } from "@klaytn/contracts/KIP/token/KIP7/extensions/KIP7Capped.sol";
+import { KIP7 } from "@klaytn/contracts/KIP/token/KIP7/KIP7.sol";
+
+/**
+ * @dev Extension of {KIP7} that adds a cap to the supply of tokens.
+ */
+abstract contract KIP7Capped is KIP7 {
+  uint256 private immutable _cap;
+
+  function _setCap() internal virtual {
+    require(cap_ > 0, "KIP7Capped: cap is 0");
+    _cap = cap_;
+  }
+
+  /**
+   * @dev Returns the cap on the token's total supply.
+   */
+  function cap() public view virtual returns (uint256) {
+    return _cap;
+  }
+
+  /**
+   * @dev See {KIP7-_mint}.
+   */
+  function _mint(address account, uint256 amount) internal virtual override {
+    require(KIP7.totalSupply() + amount <= cap(), "KIP7Capped: cap exceeded");
+    super._mint(account, amount);
+  }
+}
